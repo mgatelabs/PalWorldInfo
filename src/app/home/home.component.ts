ffdc26f876fb37f9ec9898704a10567be75bc46b
Michael Fuller <mmgate@hotmail.com>
2024-02-06 19:52:45 -0600

    2024.02.06
    1. iOS Fixes
    2. Hamburger menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PalData } from '../pal-data';
 import { PalsService } from '../pals.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-home',
@@ -9,8 +10,36 @@ import { PalsService } from '../pals.service';
 })
 export class HomeComponent implements OnInit {
   palData: PalData = new PalData();
+  numberOfColumns: number = 4;
 
-  constructor(private palsService: PalsService) {
+
+  constructor(private palsService: PalsService, breakpointObserver: BreakpointObserver) {
+
+    breakpointObserver.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.numberOfColumns = 1;
+      }
+    });
+
+    breakpointObserver.observe([
+      Breakpoints.Medium
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.numberOfColumns = 2;
+      }
+    });
+
+    breakpointObserver.observe([
+      Breakpoints.Large,
+      Breakpoints.XLarge
+    ]).subscribe(result => {
+      if (result.matches) {
+        this.numberOfColumns = 3;
+      }
+    });
 
   }
 
